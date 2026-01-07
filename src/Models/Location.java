8477a7f7110dffc203e8399d5daeba24d7a77ca4
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-07 17:05:40 +0100

    feat: add Item and NPC model classes; update Location class with new fields

--- src/Models/Location.java
+++ src/Models/Location.java
@@ -1,7 +1,13 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Location {
 
-
+    private String name;
+    private String description;
+    private List<Item> locationItems = new ArrayList<>();
+    private
 
 }

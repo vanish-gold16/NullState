664f10053b1b015cde07fa50a040abc7a825432a
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-07 17:07:10 +0100

    feat: enhance Location class with NPC list and exits map

--- src/Models/Location.java
+++ src/Models/Location.java
@@ -1,6 +1,7 @@
 package Models;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Location {
@@ -8,6 +9,7 @@ public class Location {
     private String name;
     private String description;
     private List<Item> locationItems = new ArrayList<>();
-    private
+    private List<NPC> npcs = new ArrayList<>();
+    private HashMap<String, Location> exits = new HashMap<>();
 
 }

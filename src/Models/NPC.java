5f6726fc5099a59c4026413627b1d86de83b9c3d
vanish_gold <mitrofanovvana20082@gmail.com>
2026-01-12 10:57:10 +0100

    feat: finish interaction() in NPC.java

--- src/Models/NPC.java
+++ src/Models/NPC.java
@@ -1,27 +1,71 @@
 package Models;
 
+import ENUMs.NPCs;
+
+import java.util.Random;
+
 public class NPC {
 
-    private String name;
+    private Random random = new Random();
+
+    private NPCs currentNPC;
     private String description;
     private Location location;
 
-    public NPC(String name, String description, Location location) {
-        this.name = name;
+    public NPC(NPCs currentNPC, String description, Location location) {
+        this.currentNPC = currentNPC;
         this.description = description;
         this.location = location;
     }
 
+    /**
+     * interaction patterns for non-written dialogues
+     */
     public void interaction(){
-
+        if(currentNPC == NPCs.BESTIE){
+            System.out.println("Hey, I'm busy now. Talk to me later");
+        }
+        else if(currentNPC == NPCs.VIKTOR){
+            System.out.println("VEX, go and do something with your biochip! Now!");
+        }
+        else if(currentNPC == NPCs.KOWALSKI){
+            System.out.println("Good luck with your work!");
+        }
+        else if(currentNPC == NPCs.BEZDOMOVEC){
+            int replica = random.nextInt(1, 4);
+            if(replica == 1){
+                System.out.println("Spare some credits for a poor soul");
+            }
+            else if(replica == 2){
+                System.out.println("Hard path awaits you...");
+            }
+            else{
+                System.out.println("Life is tough in this city");
+            }
+        }
+        else if(currentNPC == NPCs.ENEMIES){
+            int replica = random.nextInt(1, 4);
+            if(replica == 1){
+                System.out.println("You'll regret coming here!");
+            }
+            else if(replica == 2){
+                System.out.println("This is our territory!");
+            }
+            else{
+                System.out.println("Prepare to fight!");
+            }
+        }
+        else{
+            System.out.println("...");
+        }
     }
 
-    public String getName() {
-        return name;
+    public NPCs currentNPC() {
+        return currentNPC;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCurrentNPC(NPCs currentNPC) {
+        this.currentNPC = currentNPC;
     }
 
     public String getDescription() {

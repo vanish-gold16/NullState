05541cb56ccbe03623bb3c8dbd216a8e97666b21
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-16 18:42:32 +0100

    feat: finish Item.java

--- src/models/Item.java
+++ src/models/Item.java
@@ -1,4 +1,48 @@
 package models;
 
 public class Item {
+
+    private String name;
+    private String description;
+    private String weight;
+    private String type;
+
+    public Item(String name, String description, String weight, String type) {
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }

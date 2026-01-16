84ca53db5e653a9569dd58bc9fe93536d12ca4c2
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-16 19:03:27 +0100

    feat: finish ItemDAO.java

---
+++ src/dao/ItemDAO.java
@@ -0,0 +1,64 @@
+package dao;
+
+import com.google.gson.Gson;
+import models.Item;
+import models.Location;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class ItemDAO {
+
+    private Map<String, Item> itemsDatabase;
+
+    public ItemDAO() {
+
+    }
+
+    /**
+     * Load items from a file
+     * @param filePath the path to the file
+     */
+    private void loadItemsFromFile(String filePath){
+        try{
+            String json = new String(Files.readAllBytes(Paths.get(filePath)));
+            Gson gson = new Gson();
+            ItemsWrapper itemsWrapper = gson.fromJson(json, ItemsWrapper.class);
+
+            for(ItemData data : itemsWrapper.items){
+                Item item = new Item(
+                        data.name,
+                        data.description,
+                        data.weight,
+                        data.type
+                );
+                itemsDatabase.put(data.name, item);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get item by name
+     * @param name the name of the item
+     * @return item
+     */
+    public Item getItemByName(String name){
+        return itemsDatabase.get(name);
+    }
+
+    private static class ItemsWrapper{
+        List<ItemData> items;
+    }
+
+    private static class ItemData{
+        String name;
+        String description;
+        String weight;
+        String type;
+    }
+
+}

e48adba7d20f4f7df3d0f45f4b758f8ca20b0f04
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-01-16 09:40:00 +0100

    feat: add new NPCs and update locations in JSON files

--- src/dao/NPCDAO.java
+++ src/dao/NPCDAO.java
@@ -1,4 +1,46 @@
 package dao;
 
+import com.google.gson.Gson;
+import models.NPC;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NPCDAO {
+
+    private Map<String, NPC> npcs = new HashMap<>();
+
+    public NPCDAO() {
+        loadNPCsFromFile();
+    }
+
+    /**
+     * Load NPCs from a file
+     * @param filePath the path to the file
+     */
+    private void loadNPCsFromFile(String filePath){
+        try{
+            String json = new String(Files.readAllBytes(Paths.get(filePath)));
+            Gson gson = new Gson();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public NPC getNPCByName(String name){
+        return npcs.get(name);
+    }
+
+    private static class NPCWrapper{
+
+    }
+
+    private static class NPCData{
+        String name;
+        String description;
+    }
+
 }

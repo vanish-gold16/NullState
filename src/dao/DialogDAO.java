d628e990117897521c21dd7294085debb388eae3
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-17 20:00:30 +0100

    feat: create DialogDAO.java

---
+++ src/dao/DialogDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import models.DialogNode;
+
+import java.awt.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DialogDAO {
+
+    private Map<String, Map<String, DialogNode>> allDialogs;
+
+    public DialogDAO() {
+        loadDialogs();
+    }
+
+    private void loadDialogs(){
+        Gson gson = new Gson();
+        InputStream inputStream = getClass().getResourceAsStream("/jsons/dialogs.json");
+
+        if(inputStream == null){
+            allDialogs = new HashMap<>();
+            return;
+        }
+
+        Type type = new TypeToken<Map<String, Dialog>>() {}.getType();
+        allDialogs = gson.fromJson(new InputStreamReader(inputStream), type);
+    }
+
+    public Map<String, DialogNode> getDialogForNPC(String NPCName){
+        return allDialogs.get(NPCName.toLowerCase());
+    }
+
+}

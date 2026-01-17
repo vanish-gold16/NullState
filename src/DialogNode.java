0be94a78e497146c71df22b430cd16f83a09e1df
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-17 18:53:19 +0100

    feat: add DialogNode class with fields, constructor, and accessors

---
+++ src/DialogNode.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+public class DialogNode {
+
+    private int id;
+    private String npcText;
+    private List<DialogOption> options;
+    private boolean isEnd;
+
+    public DialogNode(int id, String npcText, List<DialogOption> options) {
+        this.id = id;
+        this.npcText = npcText;
+        this.options = options;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNpcText() {
+        return npcText;
+    }
+
+    public void setNpcText(String npcText) {
+        this.npcText = npcText;
+    }
+
+    public List<DialogOption> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<DialogOption> options) {
+        this.options = options;
+    }
+
+    public boolean isEsEnd() {
+        return isEnd;
+    }
+
+    public void setIsEnd(boolean isEnd) {
+        this.isEnd = isEnd;
+    }
+}

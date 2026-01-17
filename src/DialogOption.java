5323f6dd027191f73900c344602a87e282feb0b5
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-17 18:51:04 +0100

    feat: create fields, constructor and setters and getters in DialogOption.java

--- src/DialogOption.java
+++ src/DialogOption.java
@@ -1,2 +1,26 @@
 public class DialogOption {
+
+    private String text;
+    private String nextDialogNodeId;
+
+    public DialogOption(String text, String nextDialogNodeId) {
+        this.text = text;
+        this.nextDialogNodeId = nextDialogNodeId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getNextDialogNodeId() {
+        return nextDialogNodeId;
+    }
+
+    public void setNextDialogNodeId(String nextDialogNodeId) {
+        this.nextDialogNodeId = nextDialogNodeId;
+    }
 }

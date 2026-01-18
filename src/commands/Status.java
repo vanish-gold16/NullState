8abcc6ef6edfe9d4f8ce901b388203f56a960b88
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-18 13:35:36 +0100

    feat: finish Status.java

--- src/commands/Status.java
+++ src/commands/Status.java
@@ -1,13 +1,28 @@
 package commands;
 
+import main.CommandManager;
+
 public class Status implements Command{
+
+    private CommandManager commandManager;
+
     @Override
     public String execute() {
-        return "";
+        StringBuilder playerStatus = new StringBuilder();
+        playerStatus.append("---- Status Hráče ----\n");
+        playerStatus.append(commandManager.getPlayer().getInventory()).append(" inventář\n");
+        playerStatus.append(commandManager.getPlayer().getEddie()).append(" eddies\n");
+        playerStatus.append(commandManager.getPlayer().getCyberpsychosisLevel()).append(" cyberpsychosis level\n");
+
+        return playerStatus.toString();
     }
 
     @Override
     public boolean exit() {
         return false;
+    }
+
+    public Status(CommandManager commandManager) {
+        this.commandManager = commandManager;
     }
 }

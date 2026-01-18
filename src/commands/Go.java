fb2363cfd111a9b6c541e2e4132eb492860ce06c
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-18 21:03:00 +0100

    feat: create showAvailivleExits() in Go.java

--- src/commands/Go.java
+++ src/commands/Go.java
@@ -1,13 +1,40 @@
 package commands;
 
+import main.CommandManager;
+import models.Location;
+
+import java.util.Map;
+
 public class Go implements Command{
+
+    private CommandManager commandManager;
+
     @Override
     public String execute() {
+        String args = commandManager.getScanner().nextLine().trim().toLowerCase();
+
+        if(args.isEmpty()) return
+
         return "";
     }
 
     @Override
     public boolean exit() {
         return false;
+    }
+
+    private String showAvailibleExits() {
+        Location currentLocation = commandManager.getCurrentLocation();
+        Map<String, Location> exits = currentLocation.getExits();
+
+        if(exits == null || exits.isEmpty()) return "V této lokaci nejsou žádné východy.";
+
+        StringBuilder stringBuilder = new StringBuilder("Dostupné východy:\n");
+        stringBuilder.append(String.join(",", exits.keySet()));
+        return stringBuilder.toString();
+    }
+
+    public Go(CommandManager commandManager) {
+        this.commandManager = commandManager;
     }
 }

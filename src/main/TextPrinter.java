cfc87d05cc5fa345bcf5d2458c371042cc8dcdfd
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-22 19:50:26 +0100

    feat: implement text delay in TextPrinter.java

--- src/main/TextPrinter.java
+++ src/main/TextPrinter.java
@@ -2,6 +2,16 @@ package main;
 
 public class TextPrinter {
 
-
+    public static void type(String text){
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println(text.charAt(i));
+            try{
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println();
+    }
 
 }
